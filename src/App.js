9d2c5a49c0d365293515746e94abbed5faab551b
John Park <park.hokyun@gmail.com>
2022-01-19 23:16:22 -0500

    login works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  var user = {
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: ''
+  }
+  
   return (
+    //The below is actually JSX not JS
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Test for Sign up</h1>
+        <input type="text" name="firstName" placeholder="First name" onChange={(e) => user.firstname = e.target.value}/>
+        <input type="text" name="lastName" placeholder="Last name" onChange={(e) => user.lastname = e.target.value}/>
+        <input type="text" name="email" placeholder="Email" onChange={(e) => user.email = e.target.value}/>
+        <input type="text" name="password" placeholder="password" onChange={(e) => user.password = e.target.value}/>
+        <button onClick={() =>{ 
+          console.log(user)
+          console.log(JSON.stringify(user))
+          var test = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(user)
+          }
+          console.log(test)
+          fetch('/api/sign-up', test).then(() => {console.log("hi")})
+          }}>Sign up</button>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <h1>Test for Login</h1>      
+        <input type="text" name="email" placeholder="Email" />
+        <input type="text" name="password" placeholder="password" />
+      </div>
+      <button onClick={() =>{console.log("clicked.")} }>Login</button>
     </div>
   );
 }
